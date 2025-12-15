c6b1140d24ac12cac68705699d7f727370967d85
pixOS Builder <builder@pixos.dev>
2025-12-15 01:37:56 +0000

    refactor(hardware): Decouple hardware-configuration from public flake to ensure portability and public safety.

--- configuration.nix
+++ configuration.nix
@@ -101,49 +101,15 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-  # Allow root login via SSH
-  services.openssh.settings.PermitRootLogin = "yes";
-  # Open ports in the firewall.
+  # Deny password-based root login for security. Use 'pixos' user + sudo
+# Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 22 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # --- Static Network Configuration ---
-  networking.useDHCP = false;
-  networking.interfaces.ens18.useDHCP = false;
-
-  networking.defaultGateway = "10.10.10.1";
-  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
-
-  networking.interfaces.ens18.ipv4.addresses = [ {
-    address = "10.10.10.2";
-    prefixLength = 24;
-  } ];
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "24.11"; # Did you read the comment?
+  #Original System Install Version
+  system.stateVersion = "24.11"; #See Legacy OS documentation if needed -- This line has almost 0 impact on system function. It's more of a 'where did I come from' for devs/ community
 
 }
 
